e058455487fc5a8a69819a41151829f72c0373f7
maximillian1508 <maximillian1508@gmail.com>
2025-11-20 13:40:16 +0800

    feat: update image size and add preload to css

--- src/components/AgentProfile.tsx
+++ src/components/AgentProfile.tsx
@@ -175,7 +175,10 @@ export const AgentProfile: FC<AgentProfileProps> = ({ agent, domain, accountId, 
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
+        <noscript>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" type="text/css" />
+        </noscript>
         <Style>{css`
           * {
             margin: 0;
